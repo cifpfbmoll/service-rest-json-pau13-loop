7d12f283c1f24d7e5e6ec3990a355908ab824d0c
pau <pllinas@cifpfbmoll.eu>
2021-05-19 21:25:30 +0200

    build: :tada: Start rest service quakus project

---
+++ src/test/java/edu/pingpong/quarkus/NativeRestServiceIT.java
@@ -0,0 +1,9 @@
+package edu.pingpong.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeRestServiceIT extends RestServiceTest {
+
+    // Execute the same tests but in native mode.
+}
